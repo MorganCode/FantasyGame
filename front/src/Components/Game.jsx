b54193ebc30032e646d866c9d45076252055308a
MorganCode <morgan.and.code@gmail.com>
2018-12-15 11:33:13 +0100

    mise en place de phaser3

--- front/src/Components/Game.jsx
+++ front/src/Components/Game.jsx
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
+import Phaser from 'phaser';
+import LevelHome from '../GameEngine/LevelHome'
 
 class Game extends Component {
+	constructor(props) {
+		super(props);
+		this.phaserContainer = React.createRef();
+  }
+  
+	// Configuration de la game
+
+	componentDidMount() {
+		let config = {
+			type: Phaser.AUTO,
+			width: 800,
+			height: 600,
+			physics: {
+				default: 'arcade',
+				arcade: {
+					debug: false,
+					gravity: { y: 250 },
+				},
+			},
+			backgroundColor: '#808080',
+			scene: new LevelHome({}),
+
+			parent: this.phaserContainer,
+		};
+		new Phaser.Game(config);
+	}
+
 	render() {
-		return <div />;
+		return (
+			<div>
+				<div ref={this.phaserContainer} />
+			</div>
+		);
 	}
 }
 
---
+++ front/src/GameEngine/LevelHome.js
@@ -0,0 +1,18 @@
+import { Scene } from 'phaser';
+
+export default class LevelHome extends Scene {
+	constructor(config) {
+		const sceneConfig = {};
+		super(sceneConfig);
+		this.config = config;
+  }
+  
+  // Function du jeu
+
+	preload() {}
+
+	create() {}
+
+	update() {}
+		
+}
